694acf798667698827e69957e4064e4c91b65fd7
Rachel Hamby <tnthreat33@gmail.com>
2022-12-20 19:52:02 -0500

    commit tree 17f7400aaa328abc5a3355bfeaca6e04b51e3075

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,9 +1,21 @@
+
 function findFirstDuplicate(arr) {
-  // type your code here
+  let seen = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (seen.has(arr[i])) {
+      return arr[i];
+    } else {
+      seen.add(arr[i]);
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
+console.log("Expecting: -1")
+console.log("=>", findFirstDuplicate([1,-1,0]))
+
   console.log("Expecting: 3");
   console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
 
@@ -16,4 +28,7 @@ if (require.main === module) {
 module.exports = findFirstDuplicate;
 
 // Please add your pseudocode to this file
+//look through array and find the first number that is duplicated. 
+//iterate through the irray with the each number until you find a match 
+
 // And a written explanation of your solution
